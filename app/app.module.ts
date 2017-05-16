8797b32102fb6351a4c5c7a6b0ebeba975cac167
Artem Voloshin <voloshin.artem.a@gmail.com>
2017-05-16 11:39:11 +0300

    import component to app.module, declare it define first component with basic template use new component tag in index.html

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,10 +1,18 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser"
+import {AppComponentComponent} from "./app-component"
 
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [],
-    bootstrap: []
+    // array of imports to Module
+    imports: [
+        BrowserModule
+    ],
+    // array of declared components (will able to use in all place of code)
+    declarations: [
+        AppComponentComponent
+    ],
+    // initial module (execute firstly)
+    bootstrap: [AppComponentComponent]
 })
 
 export class AppModule {}
