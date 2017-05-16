fd9068c1f7756d040ed0d64e63dbbf1442bfde9e
Artem Voloshin <voloshin.artem.a@gmail.com>
2017-05-16 11:22:20 +0300

    initial project files

---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by AVoloshin on 13.05.2017.
+ */
+import {platformBrowserDynamic} from "@angular/platform-browser-dynamic"
+
+import {AppModule} from "./app.module"
+
+platformBrowserDynamic().bootstrapModule(AppModule)
