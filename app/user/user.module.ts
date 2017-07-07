caeb3c9fddb90cfd4df817e4afda435d7a05d67b
Artem Voloshin <voloshin.artem.a@gmail.com>
2017-07-07 16:09:19 +0300

    add template-based-form (user login component)
    + use FormsModule
    + use (ngModel) data-binding
    + use (ngSubmit)

--- app/user/user.module.ts
+++ app/user/user.module.ts
@@ -7,10 +7,12 @@ import {RouterModule} from "@angular/router"
 import {userRoutes} from "./user.routes";
 import {ProfileComponent} from "./profile.component"
 import {LoginComponent} from "./login.component";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         RouterModule.forChild(userRoutes) // in AppModule use RouterModule.forRoot, in FeatureModule - use RouterModule.forChild
     ],
     declarations: [
