724ad7d3de2845afff6ee24a5fbd3c6b99b1b4da
Artem Voloshin <voloshin.artem.a@gmail.com>
2017-07-09 23:08:49 +0300

    add Authentication service (hardcoded for now)
    + login page (emulate log-in)

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -1,10 +1,22 @@
 import {Component} from "@angular/core";
+import {AuthService} from "./auth.service"
+import {Router} from "@angular/router";
 
 @Component({
     templateUrl: 'app/user/login.component.html'
 })
 export class LoginComponent {
+    constructor(private authService: AuthService, private router: Router){
+
+    }
+
     login(data){
         console.log(data);
+        this.authService.loginUser(data.name, data.password);
+        this.router.navigate(['/events'])
+    }
+
+    goBack(){
+        this.router.navigate(['/events'])
     }
 }
